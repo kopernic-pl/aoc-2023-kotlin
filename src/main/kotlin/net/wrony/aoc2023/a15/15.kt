6fe58e565e3cb0f005062a0b50fbdcb6d6ba57ed
Mateusz Wronski <mateusz.wronski@smartbear.com>
2023-12-15 23:02:36 +0100

    15 part 1

---
+++ src/main/kotlin/net/wrony/aoc2023/a15/15.kt
@@ -0,0 +1,18 @@
+package net.wrony.aoc2023.a15
+
+import kotlin.io.path.Path
+import kotlin.io.path.readText
+
+
+fun String.hASH(): Int {
+    return this.toCharArray().fold(0) { acc, c ->
+        ((acc + c.code) * 17) % 256
+    }
+}
+
+fun main() {
+    Path("src/main/resources/15.txt").readText().split(",").also {
+            it.map(String::hASH).sum().let { sum -> println(sum) }
+        }
+
+}
