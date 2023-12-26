af582cacfa833204b39a6c36ce4a0642e6f5e123
Mateusz Wronski <mateusz.wronski@smartbear.com>
2023-12-26 17:15:16 +0100

    19 part 2 :yeah:

--- src/main/kotlin/net/wrony/aoc2023/a19/workflows.kt
+++ src/main/kotlin/net/wrony/aoc2023/a19/workflows.kt
@@ -1,13 +1,15 @@
 package net.wrony.aoc2023.a19
 
+import arrow.core.Tuple4
 import kotlin.io.path.Path
 import kotlin.io.path.readLines
 
 
 data class Rule(val name: String, val rules: List<String>) {
 
+    val default = rules.last()
+
     fun match(p: Part): String {
-        val default = rules.last()
         return rules.dropLast(1).firstOrNull { r ->
             val toCompare = when {
                 r.startsWith("x") -> p.x
@@ -22,7 +24,7 @@ data class Rule(val name: String, val rules: List<String>) {
                 else -> throw IllegalArgumentException("Unknown rule $r")
             }
             val value = r.split(":").first().drop(2).toInt()
-           comparator(toCompare, value)
+            comparator(toCompare, value)
         }?.split(':')?.last() ?: default
     }
 
@@ -43,36 +45,151 @@ data class Part(val x: Int, val m: Int, val a: Int, val s: Int) {
 
     companion object {
         fun parse(str: String): Part {
-            val (x, m, a, s) = str.drop(1).dropLast(1).split(",").map { it.split("=").last().toInt() }
+            val (x, m, a, s) = str.drop(1).dropLast(1).split(",")
+                .map { it.split("=").last().toInt() }
             return Part(x, m, a, s)
         }
     }
 }
+
+typealias Range4d = Tuple4<IntRange, IntRange, IntRange, IntRange>
+
+fun Range4d.power(): ULong {
+    val (x, m, a, s) = this
+    return x.count().toULong() * m.count().toULong() * a.count().toULong() * s.count().toULong()
+}
+
+fun IntRange.remove(other: IntRange): IntRange {
+    return when {
+        this.first < other.first -> {
+            this.first..<other.first
+        }
+
+        this.last > other.last -> {
+            other.last + 1..this.last
+        }
+
+        else -> {
+            throw IllegalArgumentException("Cannot remove $other from $this")
+        }
+    }
+}
+
+fun IntRange.overlap(other: IntRange): IntRange {
+    return when {
+        this.first < other.first -> {
+            other.first..this.last
+        }
+
+        this.last > other.last -> {
+            this.first..other.last
+        }
+
+        else -> {
+            throw IllegalArgumentException("Cannot remove $other from $this")
+        }
+    }
+}
+
+fun narrowDown(rule: Rule, inRange: Range4d, maxSize: Int): Set<Pair<String, Range4d>> {
+    val out = mutableSetOf<Pair<String, Range4d>>()
+    val rules = rule.rules
+
+    rules.fold(inRange) { range, r ->
+        when {
+            r.contains(':') -> {
+                val (matcher, target) = r.split(':')
+                val reference = matcher.drop(2).toInt()
+
+                val matchingRange = when {
+                    matcher.contains("<") -> {
+                        0..<reference
+                    }
+
+                    matcher.contains(">") -> {
+                        reference + 1..maxSize
+                    }
+
+                    else -> throw IllegalArgumentException("Unknown rule $r")
+                }
+
+                val (x, m, a, s) = range
+
+                val (narrowedRangeForCurrentTarget, narrowedRangeForNextStep) = when (matcher.first()) {
+                    'x' -> {
+                        range.copy(first = x.overlap(matchingRange)) to range.copy(first = x.remove(matchingRange))
+                    }
+
+                    'm' -> {
+                        range.copy(second = m.overlap(matchingRange)) to range.copy(second = m.remove(matchingRange))
+                    }
+
+                    'a' -> {
+                        range.copy(third = a.overlap(matchingRange)) to range.copy(third = a.remove(matchingRange))
+                    }
+
+                    's' -> {
+                        range.copy(fourth = s.overlap(matchingRange)) to range.copy(fourth = s.remove(matchingRange))
+                    }
+
+                    else -> throw IllegalArgumentException("Unknown rule $r")
+                }
+                out.add(target to narrowedRangeForCurrentTarget)
+
+                narrowedRangeForNextStep
+            }
+
+            else -> {
+                out.add(r to range)
+                range
+            }
+        }
+    }
+    return out
+}
+
 fun main() {
-    Path("src/main/resources/19.txt").readLines().let {list ->
+    Path("src/main/resources/19.txt").readLines().let { list ->
         // find index of line that is empty
         list.indexOfFirst { it.isBlank() }.let { idx ->
             // split into rules and elements
-            list.subList(0, idx).map(Rule::parse) to list.subList(idx + 1, list.size).map(Part::parse)
+            list.subList(0, idx).map(Rule::parse).associateBy { it.name } to list.subList(
+                idx + 1, list.size
+            ).map(Part::parse)
         }
-    }.also {
-        (rules, parts) ->
+    }.also { (rules, parts) ->
         // load elements from rules list to map by rule name
-        val rulesMap = rules.associateBy { it.name }
 
-        parts.groupBy {el ->
+        parts.groupBy { el ->
             var currentRule = "in"
 
             while (true) {
-                val rule = rulesMap[currentRule]!!
+                val rule = rules[currentRule]!!
                 val res = rule.match(el)
                 if (res == "A" || res == "R") {
-                    println("Element $el accepted or rejected with $res")
                     return@groupBy res
                 }
                 currentRule = res
             }
         }["A"]!!.sumOf { it.rating() }.let { println("Part 1: $it") }
-    }
-        //.also { () }
+    }.let { (rules, _) ->
+        val maxSize = 4000
+        val r = Range4d(1..maxSize, 1..maxSize, 1..maxSize, 1..maxSize)
+
+        val state = narrowDown(rules["in"]!!, r, maxSize).toMutableSet()
+        val accepted = mutableSetOf<Range4d>()
+
+        while (state.isNotEmpty()) {
+            val e = state.first()
+            state.remove(e)
+            val newRules = narrowDown(rules[e.first]!!, e.second, maxSize)
+
+            accepted.addAll(newRules.filter { (target, _) -> target == "A" }.map { it.second })
+
+            state.addAll(newRules.filter { (target, _) -> target != "R" && target != "A" })
+        }
+        println("Part 2: ${accepted.sumOf { range -> range.power() }}")
+
+
+    }.let { println(it) }
 }
--- src/test/kotlin/net/wrony/aoc2023/a19/RuleTest.kt
+++ src/test/kotlin/net/wrony/aoc2023/a19/RuleTest.kt
@@ -1,7 +1,6 @@
 package net.wrony.aoc2023.a19
 
-import org.junit.jupiter.api.Assertions.*
-import kotlin.test.Test
+import kotlin.test.*
 
 
 class RuleTest {
@@ -32,5 +31,57 @@ class RuleTest {
         val part = Part(99, 3, 1, 6)
         val rule = Rule("test", listOf("x<4:A", "m>4:B", "a>4:C", "s>5:D", "default"))
         assertEquals("D", rule.match(part))
+    }
+
+
+    @Test
+    fun shouldSubtractRanges() {
+        val inRange = 1..4000
+        assertEquals(1..332, inRange.remove(333..4000))
+    }
+
+    @Test
+    fun shouldSubtractRangesPart2() {
+        val inRange = 1..4000
+        assertEquals(457..4000, inRange.remove(1..456))
+    }
+
+    @Test
+    fun shouldSubtractRangesPart3() {
+        val inRange = 234..1234
+        assertEquals(457..1234, inRange.remove(1..456))
+    }
+
+    @Test
+    fun shouldCalcOverlap(){
+        val inRange = 1..4000
+        assertEquals(1..456, inRange.overlap(1..456))
+    }
+
+    @Test
+    fun shouldCalcOverlap2(){
+        val inRange = 1..3000
+        assertEquals(2500..3000, inRange.overlap(2500..4000))
+    }
+
+    @Test
+    fun shouldCalcOverlap3(){
+        val inRange = 200..500
+        assertEquals(200..299, inRange.overlap(1..299))
+    }
+
+    @Test
+    fun testingNarrowDown() {
+        val maxSize = 4000
+        narrowDown(
+            Rule("name", listOf("x<1000:A", "m<333:B", "a>400:C", "s>2000:D", "default")),
+            Range4d(1..maxSize, 1..maxSize, 1..maxSize, 1..maxSize), maxSize
+        ).let {
+            assertEquals("A", it.first().first)
+            assertEquals(Range4d(1..999, 1..maxSize, 1..maxSize, 1..maxSize), it.first().second)
+
+            assertEquals("default", it.last().first)
+            assertEquals(Range4d(1000..maxSize, 333..maxSize, 1..400, 1..2000), it.last().second)
+        }
     }
 }
